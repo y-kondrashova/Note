65c9c49fb7a553d6a907f2bcb248f82debc6c39e
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-11-29 12:20:09 +0200

    Add models and user app
    Models:
     - Tag,
     - Folder
     - Note
     - User (AbstractUser)
    
     Add db structure schema notes_db_structure.drawio

--- note_app/admin.py
+++ note_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from note_app.models import Tag, Folder, Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ["title", "folder", "last_updated_at"]
+    list_filter = ["title", "tags", "folder", "last_updated_at"]
+    search_fields = ["title"]
+
+
+admin.site.register(Folder)
+admin.site.register(Tag)
--- note_app/models.py
+++ note_app/models.py
@@ -1,3 +1,42 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Folder(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name="notes"
+    )
+    folder = models.ForeignKey(
+        Folder,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="notes"
+    )
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
